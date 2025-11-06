f66d080286fc98d9c0091df361449b02cfa71ecc
ngel <ngel@protonmail.com>
2025-11-06 12:48:04 +0100

    chore: Factory bot support file refactored

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,7 +1,14 @@
+require 'ffaker'
 require 'factory_bot_rails'
 
 FactoryBot.definition_file_paths = [File.expand_path('../factories', __dir__)]
-FactoryBot.find_definitions
+begin
+  FactoryBot.find_definitions
+rescue Exception => e
+  warn "FactoryBot failed to load definitions: #{e.class}: #{e.message}"
+  warn e.backtrace.join("\n")
+  raise
+end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
